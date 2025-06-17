714ab02fadee07712bba374cd55f3e07f93b8b7d
IsaacCanoH <isac.c.2586@gmail.com>
2025-06-16 21:15:54 -0600

    Correccion H

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -3,7 +3,7 @@ import LoginForm from "./LoginForm"
 import LoginHeader from "./LoginHeader"
 import { login } from "../../services/authService"
 import "bootstrap/dist/css/bootstrap.min.css"
-import styles from "../../styles/Login.module.css"
+import styles from "../../styles/login.module.css"
 import { useLoader } from "../../context/LoaderContext"
 
 const LoginPage = () => {
@@ -48,7 +48,7 @@ const LoginPage = () => {
     <div className="container-fluid p-0 min-vh-100">
       <div className="row g-0 min-vh-100">
         <LoginHeader styles={styles} />
-        <div className="col-lg-7 col-12 d-flex align-items-center justify-content-center py-4 py-lg-0">
+        <div className="col-lg-7 col-12 d-flex align-items-start justify-content-center pt-3 pt-lg-4 pb-4">
           <div className="w-100 px-3 px-sm-4 px-lg-5">
             <LoginForm
               formData={formData}
