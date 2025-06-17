9ecd2ced98cc842e981d2c61a8f3e658930e351d
IsaacCanoH <isac.c.2586@gmail.com>
2025-06-16 22:55:00 -0600

    Correccion H

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -46,7 +46,7 @@ const LoginPage = () => {
 
   return (
     <div className="container-fluid p-0 min-vh-100">
-      <div className="row g-0 min-vh-100">
+      <div className={`row g-0 ${styles["login-row"]}`}>
         <LoginHeader styles={styles} />
         <div className="col-lg-7 col-12 d-flex align-items-center justify-content-center py-4 py-lg-0">
           <div className="w-100 px-3 px-sm-4 px-lg-5">
