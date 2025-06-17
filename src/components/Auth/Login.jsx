8495e3d717724a887b9ab2ecf7c1e2913c286449
IsaacCanoH <isac.c.2586@gmail.com>
2025-06-16 23:05:49 -0600

    Correccion H

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -49,7 +49,7 @@ const LoginPage = () => {
       <div className="row g-0 min-vh-100">
         <LoginHeader styles={styles} />
         <div className="col-lg-7 col-12 d-flex justify-content-center py-3 py-lg-0 align-items-start align-items-lg-center">
-          <div className="w-100 pt-2 px-3 px-sm-4 px-lg-5">
+          <div className="w-100 pt-0 px-3 px-sm-4 px-lg-5">
             <LoginForm
               formData={formData}
               handleChange={handleChange}
